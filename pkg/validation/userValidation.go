9c34e79ba835b2cb51cc552d28202741862d9a85
j-talev <j.talev@outlook.com>
2024-06-26 23:38:44 +1000

    first name validation

--- pkg/validation/userValidation.go
+++ pkg/validation/userValidation.go
@@ -1,23 +1,44 @@
 package validation
 
 import (
+	"strconv"
+	"strings"
+	"unicode"
+
 	"github.com/jtalev/gpg-staff-portal/pkg/entities"
 )
 
 func ValidateEmployeeId(id int) Result {
 	result := Result{IsSuccessful: true, Msg: ""}
+	str := strconv.Itoa(id)
 	
-	if len(string(rune(id))) != 7 {
-		result = Result{IsSuccessful: false, Msg: "Employee id should be 7 digits long"}
+	if len(str) != 7 {
+		result = Result{IsSuccessful: false, Msg: "employee id should be 7 digits long"}
 	}
 
 	return result
 }
 
-func ValidateUser(user entities.User) []Result {
-	results := make([]Result, 0)
+func ValidateFirstName(name string) Result {
+	result := Result{IsSuccessful: true, Msg: ""}
+	name = strings.Trim(name, " ")
+
+	if len(name) > 25 {
+		result = Result{IsSuccessful: false, Msg: "first name to long"}
+	}
+	for _, c := range name {
+		if unicode.IsDigit(c) {
+			result = Result{IsSuccessful: false, Msg: "fist name contains digits"}
+		}
+	}
+
+	return result
+}
+
+func ValidateUser(user entities.User) map[string]Result {
+	results := map[string]Result{}
 	
-	results = append(results, ValidateEmployeeId(user.EmployeeId))
+	results["Employee Id"] = ValidateEmployeeId(user.EmployeeId)
 
 	return results
 }
--- test/validation_test/userValidation_test.go
+++ test/validation_test/userValidation_test.go
@@ -1,6 +1,7 @@
 package validation_test
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
@@ -16,7 +17,7 @@ func TestValidateUser(t *testing.T) {
 	user := entities.User{
 		
 		Uid:          0,
-		EmployeeId:   000000,
+		EmployeeId:   1920321,
 		FirstName:    "Robbie",
 		LastName:     "Marsh",
 		Email:        "robbieLad@outlook.com",
@@ -36,6 +37,34 @@ func TestValidateUser(t *testing.T) {
 		if r.Msg != test.Msg {
 			t.Errorf("Msg not as expected: want=%s, got=%s",
 					test.Msg, r.Msg)
+		}
+	}
+}
+
+func TestFailedValidation(t *testing.T) {
+	expected := map[string]validation.Result{
+		"Employee Id": {IsSuccessful: false, Msg: "employee id should be 7 digits long"},
+	}
+
+	user := entities.User{
+		
+		Uid:          0,
+		EmployeeId:   19203213,
+		FirstName:    "Robbie",
+		LastName:     "Marsh",
+		Email:        "robbieLad@outlook.com",
+		PasswordHash: "blsbdjksd65452",
+		CreatedAt:    time.Now(),
+		ModifiedAt:   time.Now(),
+		
+	}
+
+	result := validation.ValidateUser(user)
+
+	for i := 0; i < len(result); i++{
+		if result["Employee Id"] != expected["Employee Id"] {
+			t.Errorf("Employee Id validation failed. want=%v, got %v", 
+					expected["Employee Id"], result["Employee Id"])
 		}
 	}
 }
